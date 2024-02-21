13dcd4a5fd9b4a1c66235db2cd4c964c9c2a6c08
bruno4224onurb <144866243+bruno4224onurb@users.noreply.github.com>
2024-02-21 09:29:44 -0300

    Changed class Conta to abstract and removed its original tests from class menu

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -9,23 +9,7 @@ import conta.model.ContaCorrente;
 
 public class Menu {
 	public static void main(String[] args) {
-		// teste classe Conta
-		Conta c1 = new Conta(000001, 0001, 1, 00.00f, "Maria da Silva");
-		Conta c2 = new Conta(000002, 0001, 2, 00.00f, "José Ferreira");
-
-		c1.visualizar();
-		c1.setTitular("Maria Ramona da Silva");
-		c1.depositar(4000.00f);
-		System.out.println("Você recebeu um depósito, seu novo saldo: " + c1.getSaldo());
-		boolean saque = c1.sacar(100.00f);
-		if (saque) {
-			System.out.println("Você realizou um saque, seu novo saldo:" + c1.getSaldo());
-		} else {
-			System.out.println("Você não possui saldo para realizar este saque!");
-		}
-		c1.visualizar();
-		c2.visualizar();
-
+		
 		// teste classe ContaCorrente
 		ContaCorrente cc1 = new ContaCorrente(000003, 0001, 1, 00.00f, "José Manuel Ferreira", 100.00f);
 
